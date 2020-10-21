a6a2ec735145398246e36a151a9e0fd5fa9950fc
Aguada Jura Alex David <798-aaguada@users.noreply.gitlab.gruposancorseguros.com>
2020-10-21 20:56:11 -0300

    nada

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\LaravelProyectos\laravel8\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\LaravelProyectos\laravel8\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\LaravelProyectos\ecommerce\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\LaravelProyectos\ecommerce\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
